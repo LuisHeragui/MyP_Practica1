42f4cca29799b866e6a28be35459ab30468d00f0
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2019-09-04 11:24:13 -0500

    Cambio al nombre de InterfazUsuario.

--- src/Observador.java
+++ src/Observador.java
@@ -2,11 +2,11 @@
  * Interfaz que define el método update(). Nos permite seguir el patrón de
  * diseño Observer.
  */
-public interface InterfazUsuario{
+public interface Observador {
 
   /**
    * Método que nos permite modificar los objetos vinculados a un sujeto
    * observable una vez que este tiene algún cambio.
    */
-   public void update(int dia);
+   public void update();
 }
